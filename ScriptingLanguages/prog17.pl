ec2a6600573ba87346c5f0cfb5894c8ef0f56f57
KatipallySrija123 <69664045+KatipallySrija123@users.noreply.github.com>
2022-07-01 08:29:16 +0530

    Add files via upload

---
+++ ScriptingLanguages/prog17.pl
@@ -0,0 +1,11 @@
+print "Enter three numbers:";
+$a = <stdin>;
+$b = <stdin>;
+$c = <stdin>;
+if($a > $b and $a > $c){
+print "the largest number is $a";
+}elsif($b > $c and $b > $a){
+print "the largest number is $b";
+}else {
+print "the largest number is $c";
+}
